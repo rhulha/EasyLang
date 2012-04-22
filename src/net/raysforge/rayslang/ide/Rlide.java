90595ff7a22186c4876c1d8aa347002074b9c1a4
Raymond Hulha <java4life@gmail.com>
2012-04-22 06:56:32 +0200

    Switched the IDE to use ResourceBundles. Added English and German GUI translations.

--- src/net/raysforge/rayslang/ide/Rlide.java
+++ src/net/raysforge/rayslang/ide/Rlide.java
@@ -4,13 +4,15 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.ResourceBundle;
 
 import javax.swing.JButton;
-import javax.swing.JTree;
+import javax.swing.JMenu;
 
 import net.raysforge.easyswing.EasySplitPane;
 import net.raysforge.easyswing.EasySwing;
 import net.raysforge.easyswing.EasyTextArea;
+import net.raysforge.easyswing.EasyTree;
 import net.raysforge.rayslang.FileUtils;
 import net.raysforge.rayslang.Output;
 import net.raysforge.rayslang.RayClass;
@@ -25,22 +27,28 @@ public class Rlide implements ActionListener, Output {
 	private EasyTextArea console;
 	private JButton addToolBarItem;
 	private RayLang rayLang;
+	private EasyTree easyTree;
 
 	public Rlide() {
 
+		ResourceBundle rb = ResourceBundle.getBundle("net.raysforge.rayslang.ide.Rlide");
+		
 		es = new EasySwing("RayLang IDE", 800, 600);
 		EasySplitPane codeAndConsole = new EasySplitPane(false, 400);
 		EasySplitPane treeAndSplitPane = new EasySplitPane(true, 100);
-		treeAndSplitPane.setLeft(new JTree());
+		easyTree = new EasyTree();
+		treeAndSplitPane.setLeft(easyTree);
 		treeAndSplitPane.setRight(codeAndConsole.getSplitPane());
 		codeAndConsole.setTop(textArea = new EasyTextArea());
 		codeAndConsole.setBottom(console = new EasyTextArea());
 
 		es.getContentPane().add( treeAndSplitPane.getSplitPane(), BorderLayout.CENTER);
 		
-		es.addMenuItem("Datei");
+		JMenu fileMenuItem = es.addMenuItem(rb.getString("MenuItemFile"));
+		es.addMenuItem( fileMenuItem, rb.getString("MenuItemNewProject"), "newProject", this);
 		addToolBarItem = es.addToolBarItem("LOS");
 		addToolBarItem.addActionListener(this);
+		addToolBarItem.setActionCommand("run");
 
 		char[] completeFile = FileUtils.readCompleteFile(new File("raysrc/test/SimpleTest.ray"));
 		textArea.appendString(new String(completeFile));
@@ -60,9 +68,15 @@ public class Rlide implements ActionListener, Output {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 
-		RayLang.instance.unregisterClasses("test");
-		RayClass.parse("test", RayUtils.convertSourceToTokenList(new RaySource(textArea.getTextArea().getText().toCharArray())));
-		RayLang.runClass(rayLang.getClass("test"));
+		if( e.getActionCommand().equals("run"))
+		{
+			RayLang.instance.unregisterClasses("test");
+			RayClass.parse("test", RayUtils.convertSourceToTokenList(new RaySource(textArea.getTextArea().getText().toCharArray())));
+			RayLang.runClass(rayLang.getClass("test"));
+		} else if( e.getActionCommand().equals("newProject")) {
+			System.out.println(e.getActionCommand());
+			easyTree.addNode("Test");
+		}
 
 	}
 
