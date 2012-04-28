b85955b82d7ff6a31eec27507c4e6399814cdb72
Raymond Hulha <java4life@gmail.com>
2012-04-28 06:00:05 +0200

    Project file and folder structure with EasyTree.

--- src/net/raysforge/rayslang/ide/Rlide.java
+++ src/net/raysforge/rayslang/ide/Rlide.java
@@ -3,18 +3,25 @@ package net.raysforge.rayslang.ide;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.File;
+import java.io.IOException;
 import java.util.ResourceBundle;
 
 import javax.swing.JButton;
 import javax.swing.JMenu;
 import javax.swing.JOptionPane;
+import javax.swing.event.TreeModelEvent;
+import javax.swing.event.TreeModelListener;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreePath;
 
 import net.raysforge.easyswing.EasySplitPane;
 import net.raysforge.easyswing.EasySwing;
 import net.raysforge.easyswing.EasyTextArea;
 import net.raysforge.easyswing.EasyTree;
+import net.raysforge.easyswing.ValueForPathChangedListener;
 import net.raysforge.rayslang.FileUtils;
 import net.raysforge.rayslang.Output;
 import net.raysforge.rayslang.RayClass;
@@ -22,7 +29,10 @@ import net.raysforge.rayslang.RayLang;
 import net.raysforge.rayslang.RaySource;
 import net.raysforge.rayslang.RayUtils;
 
-public class Rlide implements ActionListener, Output {
+public class Rlide implements ActionListener, Output, ValueForPathChangedListener {
+
+	private static final String NEW_PROJECT = "newProject";
+	private static final String NEW_FILE = "newFile";
 
 	private EasySwing es;
 	private EasyTextArea textArea;
@@ -39,12 +49,20 @@ public class Rlide implements ActionListener, Output {
 
 		es = new EasySwing("RayLang IDE", 800, 600);
 		EasySplitPane codeAndConsole = new EasySplitPane(false, 400);
-		EasySplitPane treeAndSplitPane = new EasySplitPane(true, 100);
-		easyTree = new EasyTree();
-		easyTree.getJTree().setShowsRootHandles(false);
-		
-		traverse( easyTree.getRootNode(), projectsHome);
-		
+		EasySplitPane treeAndSplitPane = new EasySplitPane(true, 200);
+		easyTree = new EasyTree("Projects");
+		easyTree.setShowsRootHandles(false);
+		easyTree.setEditable();
+		easyTree.setValueForPathChangedListener(this);
+
+		traverse(easyTree.getRootNode(), projectsHome);
+
+		int row = 0;
+		while (row < easyTree.getJTree().getRowCount()) {
+			easyTree.getJTree().expandRow(row);
+			row++;
+		}
+
 		//easyTree.getJTree().setRootVisible(false);
 		treeAndSplitPane.setLeft(easyTree);
 		treeAndSplitPane.setRight(codeAndConsole.getSplitPane());
@@ -54,7 +72,9 @@ public class Rlide implements ActionListener, Output {
 		es.getContentPane().add(treeAndSplitPane.getSplitPane(), BorderLayout.CENTER);
 
 		JMenu fileMenuItem = es.addMenuItem(rb.getString("MenuItemFile"));
-		es.addMenuItem(fileMenuItem, rb.getString("MenuItemNewProject"), "newProject", this);
+		es.addMenuItem(fileMenuItem, rb.getString("MenuItemNewProject"), NEW_PROJECT, this);
+		es.addMenuItem(fileMenuItem, rb.getString("MenuItemNewFile"), NEW_FILE, this);
+
 		addToolBarItem = es.addToolBarItem("LOS");
 		addToolBarItem.addActionListener(this);
 		addToolBarItem.setActionCommand("run");
@@ -67,14 +87,13 @@ public class Rlide implements ActionListener, Output {
 		File[] fileList = directory.listFiles();
 		for (File file : fileList) {
 			DefaultMutableTreeNode newNode = easyTree.addNode(file.getName(), treeNode);
-			if( file.isDirectory())
-			{
-				traverse( newNode, file);
+			if (file.isDirectory()) {
+				traverse(newNode, file);
 			} else {
-				
+
 			}
 		}
-		
+
 	}
 
 	private void start() {
@@ -104,13 +123,31 @@ public class Rlide implements ActionListener, Output {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 
+		//System.out.println(e.getActionCommand());
+
 		if (e.getActionCommand().equals("run")) {
 			RayLang.instance.unregisterClasses("test");
 			RayClass.parse("test", RayUtils.convertSourceToTokenList(new RaySource(textArea.getTextArea().getText().toCharArray())));
 			RayLang.runClass(rayLang.getClass("test"));
-		} else if (e.getActionCommand().equals("newProject")) {
-			System.out.println(e.getActionCommand());
-			easyTree.addNode("Test");
+		} else if (e.getActionCommand().equals(NEW_PROJECT)) {
+
+			if (new File(projectsHome, "New Project").mkdir())
+				easyTree.addNode("New Project");
+			else
+				JOptionPane.showMessageDialog(null, "'New Project' folder could not be created in: " + projectsHome);
+
+		} else if (e.getActionCommand().equals(NEW_FILE)) {
+			if (easyTree.isSelected(1)) {
+				File projectFolder = new File(projectsHome, easyTree.getSelectedNode(1).toString());
+				try {
+					if (new File(projectFolder, "NewFile.easy").createNewFile())
+						easyTree.addNode("NewFile.easy", easyTree.getSelectedNode(1));
+					else
+						JOptionPane.showMessageDialog(null, "'NewFile.easy' could not be created in: " + projectFolder);
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
 		}
 
 	}
@@ -120,4 +157,24 @@ public class Rlide implements ActionListener, Output {
 		console.appendString(o + "\n");
 
 	}
+
+	@Override
+	public boolean valueForPathChanged(TreePath path, Object newValue) {
+		File fileFromTreePath = getFileFromTreePath(path);
+		boolean renameToSucceeded = fileFromTreePath.renameTo(new File(fileFromTreePath.getParentFile(), newValue.toString()));
+		return renameToSucceeded;
+	}
+
+	// this method skips the root node
+	protected File getFileFromTreePath(TreePath path) {
+		File file = projectsHome;
+
+		Object[] path2 = path.getPath();
+		for (int i = 1; i < path2.length; i++) {
+			Object object = path2[i];
+			file = new File(file, object.toString());
+		}
+		return file;
+	}
+
 }
